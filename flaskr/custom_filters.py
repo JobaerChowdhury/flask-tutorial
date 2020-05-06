20d49e1dcdbc6e8cdcfc70ce08be5c6c138b7e77
jobaer <jobaer.chowdhury@nhst.com>
2020-05-06 22:47:32 +0600

    custom filter for tag edit form

--- flaskr/custom_filters.py
+++ flaskr/custom_filters.py
@@ -3,7 +3,13 @@ import markdown
 
 def init_app(app):
     app.add_template_filter(md_to_html, name="from_markdown")
+    app.add_template_filter(separate_names_by_space, name="sep_by_space")
 
 
 def md_to_html(text):
     return markdown.markdown(text)
+
+
+def separate_names_by_space(tags):
+    names = map(lambda t: t.name, tags)
+    return " ".join(names)
